44482de9bca30163cfb92088162f3f73f8cfc146
Yassin Makrai <makrai.yassin@gmail.com>
2022-06-30 17:57:58 +0200

    ex o tirelire

--- src/coursAlex/exoTirelire/Main.java
+++ src/coursAlex/exoTirelire/Main.java
@@ -8,22 +8,16 @@ public class Main {
     public static void main(String[] args) {
 
         Tirelire tirelire = new Tirelire(10);
-        Etrenne etren = new Etrenne(1,10, tirelire,250);
-        etren.start();
-        Etrenne etren2 = new Etrenne(1,15, tirelire,250);
-        etren2.start();
+        Etrenne etren = new Etrenne(1,10, tirelire);
+        Thread t = new Thread(etren::start);
+        t.start();
+        try {
+            Thread.sleep(10_000);
+            System.out.println("thread " +Thread.currentThread().getName()+" - je met fin a l'etrenne");
+            etren.stopEtrenne();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        //region stopper l'etrenne en checkant dans le main
-//        ExecutorService executor = Executors.newFixedThreadPool(2);
-//        Future<?> f1 = executor.submit(etren::start);
-//
-//        while(tirelire.getArgent() <= 150){
-//            if(tirelire.getArgent() == 150){
-//                etren.stopEtrenne();
-//                executor.shutdownNow();
-//                break;
-//            }
-//        }
-        //endregion
     }
 }
