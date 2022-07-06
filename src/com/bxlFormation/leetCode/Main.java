04dc1e242b5cc3bfb19780d603c5b466d75ae959
Yassin Makrai <makrai.yassin@gmail.com>
2022-07-06 15:35:04 +0200

    exo leetcode longest common prefix

--- src/com/bxlFormation/leetCode/Main.java
+++ src/com/bxlFormation/leetCode/Main.java
@@ -1,14 +1,19 @@
 package com.bxlFormation.leetCode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
         int[] nums = new int[]{3,2,4};
         int target = 6;
+//        Arrays.stream(twoSum(nums,target)).forEach(System.out::println);
 
-        for (int n:twoSum(nums,target)) {
-            System.out.println(n);
-        }
-
+        String[] strs = new String[]{"darci","dog","doracecar","docar"};
+        System.out.println("end: "+longestCommonPrefix(strs));
     }
 
     public static  int[] twoSum(int[] nums, int target) {
@@ -20,5 +25,15 @@ public class Main {
             }
         }
         return new int[2];
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        String tmp = strs[0];
+        for (String str : strs) {
+            while (str.indexOf(tmp) != 0) {
+                tmp = tmp.substring(0, tmp.length() - 1);
+            }
+        }
+        return tmp;
     }
 }
